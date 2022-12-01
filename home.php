0befd143e232058a320a3c79bb19357289b88fa7
Adarsh Mohanty <am2475@srmist.edu.in>
2022-12-01 12:05:03 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/x-icon" href="logo1.png">
+</head>
+
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="logo"></div>
+            <li style="font-size: 30px;font-weight:bold;font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif; padding-left:0px">
+                Enefeti</li>
+            <li>Home</li>
+            <li>Collectors</li>
+            <li>NFT Marketplace</li>
+            <li>Features</li>
+            <li>Pages</li>
+            <li>Help</li>
+            <li>Purchases</li>
+
+            <li><a href="logout.php" style="text-decoration:none; color:white">Logout</a></li>
+
+        </div>
+    </nav>
+
+    <div id="dvImage">
+    </div>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        var images = ["enefti.jpg", "enefti2.png", "enefti3.jpg"];
+
+        function lol() {
+            var i = 0;
+            $("#dvImage").css("background-image", "url(" + images[i] + ")");
+            setInterval(function() {
+                i++;
+                if (i == images.length) {
+                    i = 0;
+                }
+                $("#dvImage").fadeOut("slow", function() {
+                    $(this).css("background-image", "url(" + images[i] + ")");
+                    $(this).fadeIn("slow");
+                });
+            }, 5000);
+        };
+        document.addEventListener('DOMContentLoaded', () => {
+            lol();
+        })
+    </script>
+</body>
+
+</html>
---
+++ read.md
@@ -0,0 +1,27 @@
+> Project Made using PHP
+> NFT landing Page
+
+> ### Please Note: 
+* Project Online Hosted Link :
+    > https://enefti-aist.000webhostapp.com/index.php
+
+# To open website first make a Database as  
+* NFT
+
+# Database contains a table , namely :
+* nft_data
+
+# Schema for the table is as follows: 
+
+* >1)nft_data : 
+    # 	Name 	        Type 	        Collation 	       		Default 	Comments 	Extra 	
+    1 	id(Primary)     int(25) 			                    No 	        None    AUTO_INCREMENT 	
+
+    2 	email 	        varchar(255) 	utf8mb4_general_ci 		No 	        None 			
+
+    3 	pass 	        varchar(255) 	utf8mb4_general_ci 		No 	        None 				
+
+    4 	names 	        varchar(255) 	utf8mb4_general_ci 		No 	        None 
+    
+
+ 
